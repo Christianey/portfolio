dd4743e69a1a2a51c235e5cd26fef77a0c063d59
Christianey <eyubechristian@gmail.com>
2022-02-06 21:45:40 +0100

    added colored radial gradients

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -1,4 +1,4 @@
-import { Flex, Text, Image } from "@chakra-ui/react";
+import { Flex, Text, Circle } from "@chakra-ui/react";
 
 const Hero = () => {
   return (
@@ -8,13 +8,30 @@ const Hero = () => {
       w="full"
       h="95vh"
       bg={"darkBlue"}
+      overflow={"hidden"}
     >
+      <Circle
+        pos={"absolute"}
+        size={"30rem"}
+        left={"50%"}
+        top={"-10%"}
+        transform={"translate(-50%, -50%)"}
+        bg="radial-gradient(circle, #00C95D 30%, transparent 70%)"
+      />
+      <Circle
+        pos={"absolute"}
+        size={"30rem"}
+        left={"50%"}
+        bottom={"-10%"}
+        transform={"translate(-50%, 50%)"}
+        bg="radial-gradient(circle, #3A91A0 30%, transparent 70%)"
+      />
       <Text color={"primary"}>Hello there, I am </Text>
       <Flex
         color={"secondary"}
         fontWeight={"bold"}
         // bgGradient="linear(to-r, primary, secondary)"
-        bg="-webkit-linear-gradient( 00C95D 0%, 3A91A0 50%)"
+        // bg="-webkit-linear-gradient( 00C95D 0%, 3A91A0 50%)"
         bg="-webkit-linear-gradient(0deg, #00C95D 0%, #3A91A0 50%)"
         sx={{
           "&": {
@@ -22,7 +39,7 @@ const Hero = () => {
             "-webkit-text-fill-color": "transparent",
           },
         }}
-        fontSize={"10rem"}
+        fontSize={["4rem", "6rem", "8rem", "10rem"]}
       >
         Fegabyte
       </Flex>
