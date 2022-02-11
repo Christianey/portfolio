07238058eafd878f0e28bc112c81f16bf596fb92
Christianey <eyubechristian@gmail.com>
2022-02-11 05:56:35 +0100

    moved stylesheet

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../constants/theme";
-import "../styles/styles.css";
+import "../public/styles.css";
 
 function MyApp({ Component, pageProps }) {
   return (
